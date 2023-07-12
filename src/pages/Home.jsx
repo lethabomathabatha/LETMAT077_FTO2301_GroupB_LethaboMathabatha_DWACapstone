7e69ae43a3c4d7293d5d2e28b518f43f140f802f
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-12 19:24:02 +0200

    added home page genres

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,34 +31,39 @@ export default function Home() {
 
   return (
     <div>
-      <nav className="home--title">
-        <p>Hello! Welcome to Pods App</p>
-        <SearchIcon />
-    </nav>
+        <nav className="home--title">
+            <p>Hello! Welcome to Pods App</p>
+            <SearchIcon />
+        </nav>
 
-    <div className="home--top-ten">
-        <aside className="home--top-title">Our Top Picks</aside>
+        <div className="home--top-ten">
+            <aside className="home--top-title">Our Top Picks</aside>
 
-        <div className="home--display-top">
-        {display.map((data) => {
-          return (
-            <div key={data.id} className="home--card">
-              <img
-                src={data.image}
-                className="home--card-image"
-                alt="podcast-image"
-              />
-              <h2 className="home--card-label">{data.title}</h2>
-              {/* <h2 className="home--card-seasons">{data.seasons} Seasons</h2>
-              <h2 className="home--card-genres"> <strong>Genres</strong> {data.genres}</h2>
-              <h4 className="home--card-description"><strong>Description</strong> {data.description}</h4>
-              <h4 className="home--card-updated"><strong>Last updated</strong> {data.updated}</h4>
-              <br /> */}
-              <br />
+            <div className="home--display-top">
+            {display.map((data) => {
+            return (
+                <div key={data.id} className="home--card">
+                    <img
+                        src={data.image}
+                        className="home--card-image"
+                        alt="podcast-image"
+                    />
+                    <h2 className="home--card-label">{data.title}</h2>
+                    {/* <h2 className="home--card-seasons">{data.seasons} Seasons</h2>
+                    <h2 className="home--card-genres"> <strong>Genres</strong> {data.genres}</h2>
+                    <h4 className="home--card-description"><strong>Description</strong> {data.description}</h4>
+                    <h4 className="home--card-updated"><strong>Last updated</strong> {data.updated}</h4>
+                    <br /> */}
+                </div>
+            );
+            })}
             </div>
-          );
-        })}
-      </div>
+        </div>
+
+    <div className="home--genres">
+        <h3 className="home--genres-title">Genres</h3>
+        
+        
     </div>
     
       
