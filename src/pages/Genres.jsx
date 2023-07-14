ad25773ed76fcfe81a194f9bc88a17517e62a30c
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-14 14:07:16 +0200

    added home page genre section

---
+++ src/pages/Genres.jsx
@@ -0,0 +1,28 @@
+// import { useEffect, useState } from "react";
+// import Button from '@mui/material/Button';
+// import { CircularProgress } from "@mui/material";
+import '/src/pages/HomeStyles.css'
+
+export default function Genres() {
+
+    return (
+        <div className="home--genres">
+            <h3 className="home--genres-title">Genres</h3>
+           
+            {/* {display.map((data) => {
+            return (
+                <div key={data.title} className="home--genres-card">
+
+                  <button><h2 className="home--genres-button"> 
+                    Genres {data.genres}
+                  </h2></button>
+                  
+
+                 
+                </div>
+            );
+            })}
+             */}
+        </div>  
+    )
+}
