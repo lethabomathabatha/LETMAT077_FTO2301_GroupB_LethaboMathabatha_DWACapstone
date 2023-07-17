265c18587bf6d4c8435fab17729bddb9faed2398
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-17 15:42:20 +0200

    added bottom nav functionality

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search"
 import '/src/pages/HomeStyles.css'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
@@ -7,12 +8,45 @@ import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 
 
 export default function BottomNav() {
+const [homeIconColor, setHomeIconColor] = useState("#3D2A75");
+
+    function sendToHome() {
+        window.location.href = "/";
+        setHomeIconColor("#3D2A75");
+
+        // change icon color
+        // document.getElementsByClassName("home--icon")[0].style.color = "#3D2A75";
+
+    }
+    function sendToLogin() {
+        window.location.href = "/login";
+    }
+
     return (
-        <div className="home--bottom-nav">
-          <HomeOutlinedIcon />
-          <SearchIcon />
-          <FavoriteBorderOutlinedIcon />
-          <PermIdentityOutlinedIcon />
+        <div className="bottom-nav">
+          <HomeOutlinedIcon 
+            className="home--icon"
+            onClick={sendToHome}
+            style={{cursor: "pointer", color: homeIconColor}}
+    
+          />
+
+          <SearchIcon 
+            className="search--icon"
+            style={{cursor: "pointer"}} 
+          />
+
+          <FavoriteBorderOutlinedIcon
+            className="favorite--icon"
+            style={{cursor: "pointer"}}
+          />
+
+          <PermIdentityOutlinedIcon 
+            className="profile--icon"
+            // onClick, take the user to /login
+            onClick={sendToLogin}
+            style={{cursor: "pointer"}}
+          />
         </div>
     )
 }
