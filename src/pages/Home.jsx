8b1949c0aa94a595422385b9d6f8cce57c588c7a
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-17 11:32:06 +0200

    added home page nav styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import LatestEps from "./LatestEps";
 import { CircularProgress } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 
 export default function Home() {
   const [display, setDisplay] = useState([]);
@@ -31,13 +31,30 @@ export default function Home() {
 
   }, []);
 
+  // greeting based on the time of day
+  // let user = JSON.parse(localStorage.getItem("user"));
+  let user = "Lethabo"
+  const time = new Date().getHours();
+  // if it's before 12pm, it's morning, if it's after 12pm but before 6pm, it's afternoon, if it's after 6pm but before 3am, it's evening
+  let greeting = "";
+  if (time >= 3 && time < 12) {
+    greeting = `Good Morning, ${user}.`;
+  } else if (time >= 12 && time < 18) {
+    greeting = `Good Afternoon, ${user}.`;
+  } else if (time >= 18 && time < 24) {
+    greeting = `Good Evening, ${user}.`;
+  } else {
+    greeting = `Good Night, ${user}.`;
+  }
+
   
 
   return (
     <div>
         <nav className="home--navigation">
-            <p className="home--title">Hello! Welcome to Pods App</p>
-            <TextField
+          <img src="../pods-logo-light.png" width="90px" className="home--logo"/>
+            <p className="home--title">{greeting}</p>
+            {/* <TextField
               className="home--search"
               
               color="secondary"
@@ -53,7 +70,7 @@ export default function Home() {
                   </InputAdornment>
                 ),
               }}
-            />
+            /> */}
         </nav>
 
         <div className="home--top-ten">
@@ -88,6 +105,7 @@ export default function Home() {
             {/* genres */}
         <Genres />    
         <LatestEps />  
+        <div className="home--bottom-nav"></div>
     </div>
   );
 }
