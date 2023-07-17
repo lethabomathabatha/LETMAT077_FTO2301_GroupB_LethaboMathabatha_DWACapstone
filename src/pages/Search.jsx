2ebd93e22681ccd90d50cb12b86213c3868b6557
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-17 16:34:18 +0200

    added search page styling

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -12,23 +12,18 @@ export default function Search() {
 
 
     return (
-        <div className="search">
-            Hello World
+        <div className="search--page">
+            <p className="search--header">Search For The Next <strong>Best Podcast </strong>You've Ever Heard</p>
 
             <TextField
-                className="home--search"
-                
+                className="home--search-field"
                 color="secondary"
-                
                 placeholder="Search"
-                size="small"
-
+                size="medium"
+                variant="standard"
                 InputProps={{ 
                 startAdornment: (
-                    <InputAdornment position="start">
                     <SearchIcon />
-
-                    </InputAdornment>
                 ),
                 }}
             /> 
