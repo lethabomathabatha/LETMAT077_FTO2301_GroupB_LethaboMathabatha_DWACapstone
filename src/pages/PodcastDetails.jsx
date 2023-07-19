63d254b2374c5c94faa8d39b58ffeea34417122b
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-19 16:55:17 +0200

    added podcast details styles

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -2,14 +2,24 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import BottomNav from '../components/BottomNav';
 import PlayCircleOutlineTwoToneIcon from '@mui/icons-material/PlayCircleOutlineTwoTone';
-import PauseTwoToneIcon from '@mui/icons-material/PauseTwoTone';
-
+// import PauseIcon from '@mui/icons-material/Pause';
+import '/src/pages/PodcastDetailsStyles.css'
+// import Button from '@mui/material/Button';
 
 
 export default function PodcastDetails() {
     const { id } = useParams();
 
     const [selectedPodcast, setSelectedPodcast] = useState();
+    // const [seasonsButtons, setSeasonsButtons] = useState([]);
+
+    // function handlePlayPause(episode) {
+    //     if (episode.play) {
+    //         episode.pause();
+    //     } else {
+    //         episode.play();
+    //     }
+    // }
 
     // fetch full info about the podcast using the id
     useEffect(() => {
@@ -19,34 +29,78 @@ export default function PodcastDetails() {
             .catch((error) => console.log(error))
     }, [id])
 
+
+
     
     return (
         <div>
-            <h4>Podcast Details for Podcast {id}</h4>
-            
-            {/* get podcast title, description, seasons, episodes within those seasons, episode titles, episode descriptions, episode numbers, episode audio files */}
             
             {selectedPodcast && (
-                <div>
-                    <h2>Title: {selectedPodcast.title}</h2>
-                    <p>Description: {selectedPodcast.description}</p>
+                <div className="details--page">
+                    <div className="details--header">
+                        <img src="../public/podcast-bg.png" alt="podcast-background" width={"100%"} className="details--header-image"/>
 
-                    {/* Get seasons from array, and display each one with its episodes, episode titles and audio files */}
-                    <p>Total Seasons: {selectedPodcast.seasons.length}</p>
+                        <h2 className="details--header-title">{selectedPodcast.title.replace(/&amp;/g, " &")}</h2>
+                        <img src={selectedPodcast.image} className="details--header-pod-image"/>
+                        <p className="details--header-description">{selectedPodcast.description}</p>
+                        <p className="details--header-seasons-genres">Now on Season {selectedPodcast.seasons.length}  | {selectedPodcast.genres}</p>
+                    </div>
+
+                    <div className="details--seasons-btns">
+                        <p>Select a season</p>
+                        {/* <Button />
+                        {seasonsButtons.map((season) => (
+                        <div key={season}>
+                            <Button 
+                            key={season}
+                            variant="contained"
+                            color="secondary"
+                            style={{
+                                color: "var(--light-purple)",
+                                fontWeight: "800",
+                                borderRadius: "15px",
+                                backgroundColor: "var(--ice-white)",
+                                fontSize: "11px",
+                                height: "35px",
+                                whiteSpace: "nowrap",
+                            }}
+                            >
+                            {season}
+                            </Button>
+                        </div>
+                    ))} */}
+
+                    </div>
                     
                     {selectedPodcast.seasons.map((season) => (
-                        <div key={season.season}>
-                            <h4>Season {season.season}</h4>
-                            <img src={season.image}  width={100}/>
-
+                        <div key={season.season} className="details--cards">
+                            <h4 className="details--cards-season-eps">Season {season.season} {season.episodes.length} Eps</h4>
+ 
                             {/* map through every season's episodes */}
                             {season.episodes.map((episode) => (
                                 <div key={episode.episode}>
-                                    <p>Episode {episode.episode}: {episode.title}</p>
-                                    <p>{episode.description}</p>
-                                    <audio controls>
+                                    <img src={season.image}  width={50} className="details--cards-image"/>
+                                    <p className="details--cards-episode">Episode {episode.episode}: {episode.title}</p>
+                                    <p className="details--cards-episode-description">{episode.description}</p>
+
+                                    <PlayCircleOutlineTwoToneIcon />
+                                    {/* {episode.paused ? (
+                                        <PlayCircleOutlineTwoToneIcon 
+                                        onClick={() => handlePlayPause(episode) }
+                                        style={{cursor: "pointer"}}
+                                    />
+                                    ) : (
+                                        <PauseIcon 
+                                            onClick={() => handlePlayPause(episode) }
+                                            style={{cursor: "pointer"}}
+                                        />
+                                    )}
+                                     */}
+
+                                    {/* this has to go */}
+                                    {/* <audio controls>
                                         <source src={episode.file} type="audio/mp3" />
-                                    </audio>
+                                    </audio> */}
                                 </div>
                             ))}
                             <br/>
