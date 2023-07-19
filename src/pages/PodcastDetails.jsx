b58c8006d269db1751a0dfbc80ea0af2c39c8b65
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-19 13:38:15 +0200

    added podcast details logic

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -1,14 +1,27 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 
 export default function PodcastDetails() {
     const { id } = useParams();
-    
+
+    const [selectedPodcast, setSelectedPodcast] = useState();
+
+    // fetch full info about the podcast using the id
+    useEffect(() => {
+        fetch(`https://podcast-api.netlify.app/shows/${id}`)
+            .then((res) => res.json())
+            .then((data) => setSelectedPodcast(data))
+            .catch((error) => console.log(error))
+    }, [id])
+
     
     return (
         <div>
-            <h1>Podcast Details {id}</h1>
-            <h2>Title: </h2>
+            <h4>Podcast Details for Podcast {id}</h4>
+            
+            {/* get podcast title, description, seasons, episodes within those seasons, episode titles, episode descriptions, episode numbers, episode audio files */}
+            
         </div>
     )
 }
