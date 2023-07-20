b36d6b8365f3d9ccce424b92faf13c3920900e68
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-20 10:42:05 +0200

    added podcast details styles

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -5,6 +5,7 @@ import PlayCircleOutlineTwoToneIcon from '@mui/icons-material/PlayCircleOutlineT
 // import PauseIcon from '@mui/icons-material/Pause';
 import '/src/pages/PodcastDetailsStyles.css'
 // import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 
 export default function PodcastDetails() {
@@ -29,9 +30,6 @@ export default function PodcastDetails() {
             .catch((error) => console.log(error))
     }, [id])
 
-
-
-    
     return (
         <div>
             
@@ -40,70 +38,49 @@ export default function PodcastDetails() {
                     <div className="details--header">
                         <img src="../public/podcast-bg.png" alt="podcast-background" width={"100%"} className="details--header-image"/>
 
-                        <h2 className="details--header-title">{selectedPodcast.title.replace(/&amp;/g, " &")}</h2>
+                            <ArrowBackIosIcon 
+                                className="details--header-back-btn"
+                                onClick={() => window.location.href = "/search"}
+                                style={{cursor: "pointer"}}
+                            />
+                            <h2 className="details--header-title">{selectedPodcast.title.replace(/&amp;/g, " &")}</h2>
+                        
+
                         <img src={selectedPodcast.image} className="details--header-pod-image"/>
                         <p className="details--header-description">{selectedPodcast.description}</p>
-                        <p className="details--header-seasons-genres">Now on Season {selectedPodcast.seasons.length}  | {selectedPodcast.genres}</p>
+                        <p className="details--header-seasons-genres">Now on Season {selectedPodcast.seasons.length}  | {selectedPodcast.genres.join(", ").replace(/All,/g, "")}</p>
                     </div>
 
                     <div className="details--seasons-btns">
                         <p>Select a season</p>
-                        {/* <Button />
-                        {seasonsButtons.map((season) => (
-                        <div key={season}>
-                            <Button 
-                            key={season}
-                            variant="contained"
-                            color="secondary"
-                            style={{
-                                color: "var(--light-purple)",
-                                fontWeight: "800",
-                                borderRadius: "15px",
-                                backgroundColor: "var(--ice-white)",
-                                fontSize: "11px",
-                                height: "35px",
-                                whiteSpace: "nowrap",
-                            }}
-                            >
-                            {season}
-                            </Button>
-                        </div>
-                    ))} */}
-
                     </div>
                     
                     {selectedPodcast.seasons.map((season) => (
-                        <div key={season.season} className="details--cards">
-                            <h4 className="details--cards-season-eps">Season {season.season} {season.episodes.length} Eps</h4>
- 
+                        <div key={season.season} className="details--section">
+                            <aside className="details--season-title"><strong>Season {season.season}</strong> {season.episodes.length} Episodes</aside>
+                            
+                            <div className="details--section-cards">
                             {/* map through every season's episodes */}
                             {season.episodes.map((episode) => (
-                                <div key={episode.episode}>
-                                    <img src={season.image}  width={50} className="details--cards-image"/>
-                                    <p className="details--cards-episode">Episode {episode.episode}: {episode.title}</p>
-                                    <p className="details--cards-episode-description">{episode.description}</p>
+                                <div key={episode.episode} className="details--show-cards">
+                                    <img src={season.image} className="details--cards-image"/>
 
-                                    <PlayCircleOutlineTwoToneIcon />
-                                    {/* {episode.paused ? (
-                                        <PlayCircleOutlineTwoToneIcon 
-                                        onClick={() => handlePlayPause(episode) }
-                                        style={{cursor: "pointer"}}
-                                    />
-                                    ) : (
-                                        <PauseIcon 
-                                            onClick={() => handlePlayPause(episode) }
-                                            style={{cursor: "pointer"}}
-                                        />
-                                    )}
-                                     */}
-
-                                    {/* this has to go */}
-                                    {/* <audio controls>
-                                        <source src={episode.file} type="audio/mp3" />
-                                    </audio> */}
+                                    <div className="details--cards-text">
+                                        <span className="details--cards-episode">Ep{episode.episode}: {episode.title}</span>
+                                        <span className="details--cards-episode-description">{episode.description}</span>
+                                        
+                                        <div className="details--podcast-play">
+                                        <span className="details--podcast-title">{selectedPodcast.title.replace(/&amp;/g, " &")}  | 43:00</span>
+                                        {/* audio file length */}
+                                        {/* <audio src={episode.file}  controls></audio> */}
+                                        <PlayCircleOutlineTwoToneIcon className="details--audio-btn"/>
+                                        </div>
+                                    </div>
+                                    
+                                    
                                 </div>
                             ))}
-                            <br/>
+                            </div>
                         </div>
                     ))}
                 </div>
