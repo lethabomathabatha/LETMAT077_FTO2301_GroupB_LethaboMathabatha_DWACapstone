bb4ef301b3fdf5a3a8121606c8d0d82f06885588
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-24 21:41:30 +0200

    add window confirm

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -4,23 +4,26 @@ import PauseIcon from '@mui/icons-material/Pause';
 import { CircularProgress } from "@mui/material";
 import GraphicEqIcon from '@mui/icons-material/GraphicEq'
 
-export default function AudioPlayer({ audioSrc, onPlayPause, selectedPodcast, episodeName, currentPlayingEpisodeName, currentPodcastImg}) {
-    const audioRef = useRef();
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [showAudioPlayer, setShowAudioPlayer] = useState(false);
-
+export default function AudioPlayer({ 
+  audioSrc, 
+  selectedPodcast, 
+  episodeName, 
+  currentPlayingEpisodeName, 
+  currentPodcastImg,
+  setCurrentPlayingEpisodeId,
+}) {
+  const audioRef = useRef();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showAudioPlayer, setShowAudioPlayer] = useState(false);
 
   const handlePlayPause = () => {
     if (isPlaying) {
       audioRef.current.pause();
-      console.log("paused");
       setShowAudioPlayer(false);
     } else {
       audioRef.current.play();
-      
       setShowAudioPlayer(true);
-      console.log("playing");
     }
     setIsPlaying((prevState) => !prevState);
   };
@@ -31,53 +34,86 @@ export default function AudioPlayer({ audioSrc, onPlayPause, selectedPodcast, ep
 
   useEffect(() => {
     audioRef.current.addEventListener("ended", () => setIsPlaying(false));
-    return() => {
-        audioRef.current.removeEventListener("ended", () => setIsPlaying(false))
+    return () => {
+      audioRef.current.removeEventListener("ended", () => setIsPlaying(false))
     }
   }, []);
 
   useEffect(() => {
+    const onPlayPause = (playing) => {
+      setIsPlaying(playing);
+    };
+
     if (onPlayPause) {
-        onPlayPause(isPlaying)
+      onPlayPause(isPlaying)
     }
-  }, [isPlaying, onPlayPause]);
+  }, [isPlaying]);
+
+  function closePlayer() {
+    // close audio player when close button is clicked
+    const confirmClose = window.confirm("Are you sure you want to close the audio player?")
+    if (confirmClose) {
+      setShowAudioPlayer(false)
+    }
+  }
+
+  useEffect(() => {
+    // Add an event listener to the beforeunload event
+    const handleBeforeUnload = (e) => {
+      if (showAudioPlayer) {
+        e.preventDefault();
+        e.returnValue = "Are you sure you want to close the audio player?";
+        return "Are you sure you want to close the audio player?";
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      return "Are you sure you want to close the audio player?";
+
+    };
+  }, [showAudioPlayer]);
+
 
   return (
     <div>
-        <div className="details--audio-player">
-            <audio
-                ref={audioRef}
-                src={audioSrc}
-                preload="metadata"
-                // onEnded={() => setIsPlaying(false)}
-                onLoadedData={handleLoadedData}
-            />
+      <div className="details--audio-player">
+        <audio
+          ref={audioRef}
+          src={audioSrc}
+          preload="metadata"
+          onLoadedData={handleLoadedData}
+        />
 
-            {/* conditionally render the play/pause buttons */}
-            {isLoading ? (
-                <CircularProgress color="secondary" size={"1.5rem"} />
-            ) : isPlaying ? (
-                <div>
-                <GraphicEqIcon style={{ opacity: "0.5" }} className="details--audio-equalizer" />
-                <PauseIcon onClick={handlePlayPause} style={{ cursor: "pointer" }} />
-                </div>
-            ) : (
-                <PlayCircleOutlineTwoToneIcon onClick={handlePlayPause} style={{ cursor: "pointer" }} />
-            )}
-        </div>
-
-        {{handlePlayPause} && (
-            <div style={{ position: "fixed", bottom: 50, left: 0, right: 0, backgroundColor: "hotpink", display: "flex", justifyContent: "center", padding: 10, flexDirection: "column" }}>
-                <p>Title : {selectedPodcast.title}</p>
-                <p>Episode : {episodeName}</p>
-                <p>{currentPlayingEpisodeName}</p>
-                <img src={currentPodcastImg} alt="podcast-image" width={"45"}/>
-                <audio controls><source src={audioSrc}  type="audio/mpeg" /></audio>
-                {/* add show title, show image, podcast name to audio player */}
-            </div>
+        {/* Conditionally render the play/pause buttons */}
+        {isLoading ? (
+          <CircularProgress color="secondary" size={"1.5rem"} />
+        ) : isPlaying ? (
+          <div>
+            <GraphicEqIcon style={{ opacity: "0.5" }} className="details--audio-equalizer" />
+            <PauseIcon onClick={handlePlayPause} style={{ cursor: "pointer" }} />
+          </div>
+        ) : (
+          <PlayCircleOutlineTwoToneIcon onClick={handlePlayPause} style={{ cursor: "pointer" }} />
         )}
+      </div>
 
-
+      {/* Display additional information when audio player is shown */}
+      {showAudioPlayer && (
+        <div className="audio--bottom-player" style={{ position: "fixed", bottom: 50, left: 0, right: 0, backgroundColor: "hotpink", display: "flex", justifyContent: "center", padding: 10, flexDirection: "column" }}>
+          <span>Title: {selectedPodcast.title}</span>
+          <span>Episode: {episodeName}</span>
+          <span>{currentPlayingEpisodeName}</span>
+          <h4>ID: {setCurrentPlayingEpisodeId}</h4>
+          <img src={currentPodcastImg} alt="podcast-image" width={45} />
+          <audio controls><source src={audioSrc} type="audio/mpeg" /></audio>
+          <button onClick={closePlayer}>Close</button>
+          {/* Add show title, show image, podcast name to audio player */}
+        </div>
+      )}
     </div>
   );
 }
