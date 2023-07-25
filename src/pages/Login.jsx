636e69e6890da4646379ffca2c5b42a6aa2154d5
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-25 16:48:27 +0200

    add bottom nav

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
 // import React from 'react';
 import TextField from '@mui/material/TextField';
 import '../pages/LoginStyles.css';
+import BottomNav from '../components/BottomNav';
 
 export default function Login() {
   return (
       <div className="login--form">
         <TextField id="standard-basic" label="Email" required variant="standard" color="secondary"/>
         <TextField id="standard-basic" label="Password" required variant="standard" color="secondary"/>
+
+        <BottomNav />
       </div>
   );
 }
