35c8be6cbe8d504eb451ea341ea5fd14943121eb
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-26 16:27:24 +0200

    add shows under genre filters

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
 import '/src/pages/HomeStyles.css'
 import { CircularProgress } from "@mui/material";
-
-
+import { Link } from "react-router-dom";
 
 export default function Genres() {
   const [genreButtons, setGenreButtons] = useState([]);
-
+  const [shows, setShows] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -19,13 +18,14 @@ export default function Genres() {
           fetch(`https://podcast-api.netlify.app/id/${show.id}`)
             .then((res) => res.json())
             .then((showData) => {
-              show.genres = showData.genres || []; // Set genres to an empty array if it's not available
+              show.genres = showData.genres || [];
               return show;
             })
         );
 
         Promise.all(genreFetchPromises)
           .then((showsData) => {
+            setShows(showsData); // Store the shows data in state
             const allGenres = showsData.reduce((genres, show) => {
               show.genres.forEach((genre) => {
                 if (!genres.includes(genre)) {
@@ -41,41 +41,73 @@ export default function Genres() {
       });
   }, []);
 
+  const handleGenreClick = (genre) => {
+    console.log(`Clicked on genre: ${genre}`);
+    const showsWithGenre = shows.filter((show) => show.genres.includes(genre));
+    console.log("Shows with this genre:", showsWithGenre);
+    // get the IDs of the shows with this genre so that we can view them on 'search/:id'
+    const showIds = showsWithGenre.map((show) => show.id);
+    console.log("Show IDs:", showIds);
+
+
+  };
+
   return (
     <div className="home--genres">
       <h3 className="home--genres-title">Genres</h3>
 
-      <div className="home--genres-button-group">  
-      {isLoading ? (
-                <div className="loading">
-                <CircularProgress color="secondary" /> 
+      <div className="home--genres-button-group">
+        {isLoading ? (
+          <div className="loading">
+            <CircularProgress color="secondary" />
+          </div>
+        ) : (
+          <>
+            {genreButtons.map((genre) => (
+              // <Link to={`/search/${episode.id.}`} key={episode.id} style={{textDecoration:"none", color:"inherit", cursor:"pointer"}}>
+             // <Link /> should take the id and take user to 'search/:id'
+            //  <Link to={`/search/${genre.showIds}`} key={genre} style={{textDecoration:"none", color:"inherit", cursor:"pointer"}}>
+
+             <div key={genre}>
+                <Button
+                  key={genre}
+                  variant="contained"
+                  color="secondary"
+                  style={{
+                    color: "var(--light-purple)",
+                    fontWeight: "800",
+                    borderRadius: "15px",
+                    backgroundColor: "var(--ice-white)",
+                    fontSize: "11px",
+                    height: "35px",
+                    whiteSpace: "nowrap",
+                    marginBottom: "20px",
+                  }}
+                  onClick={() => handleGenreClick(genre)}
+                >
+                  {genre}
+                </Button>
+
+                {/* when user clicks on a genre, there should be a list of shows that have that genre appear under the buttons */}
+                <div className="genre--results">
+                  {shows.filter((show) => show.genres.includes(genre)).map((show) => (
+                    <Link to={`/search/${show.id}`} key={show.id} style={{textDecoration:"none", color:"inherit", cursor:"pointer"}}>
+                      <div
+                        key={show.id}
+                        style={{
+                          color: "var(--lila-white)",
+                        }}
+                      >
+                        <span>{show.title}</span>
+                      </div>
+                    </Link>
+                  ))}
                 </div>
-            ) : (
-            <>  
-      {genreButtons.map((genre) => (
-        <div key={genre}>
-            <Button 
-            key={genre}
-            variant="contained"
-            color="secondary"
-            style={{
-                color: "var(--light-purple)",
-                fontWeight: "800",
-                borderRadius: "15px",
-                backgroundColor: "var(--ice-white)",
-                fontSize: "11px",
-                height: "35px",
-                whiteSpace: "nowrap",
-            }}
-            >
-            {genre}
-            </Button>
-        </div>
-      ))}
-      </>
-      )}
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
-    </div>
-  )
-  
+  );
 }
