4f9c6682f3c53ac6c2ba5c499317b86ab2c6cc4e
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-28 00:25:09 +0200

    change to getUser

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,31 @@
 import { useEffect } from 'react';
 import '../pages/LoginStyles.css';
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useNavigate } from 'react-router-dom';
+import { supabase } from '../main';
 
-const supabaseUrl = import.meta.env.VITE_REACT_APP_PODS_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_REACT_APP_PODS_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+// const supabaseUrl = import.meta.env.VITE_REACT_APP_PODS_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_REACT_APP_PODS_SUPABASE_ANON_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Login() {
   // after the user logs in, send them to the home page
   const navigate = useNavigate();
 
   useEffect(() => {
-    // send user to home page if user is logged in
-    if (supabase.auth.user()) {
+    // send user to home page if user is logged in, if not, send them to login
+    if (supabase.auth.getUser()) {
       navigate('/');
+    } else {
+      navigate('/login');
     }
-  }, [navigate]);
+
+
+    
+   });
 
   return (
     <div className="login--form">
