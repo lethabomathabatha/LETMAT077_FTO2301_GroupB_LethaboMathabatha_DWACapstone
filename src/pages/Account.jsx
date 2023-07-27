7e61cbb5fa1ae9226b05a7be59ccffe464e44fe3
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-27 21:10:16 +0200

    add account details

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,16 +1,62 @@
-import BottomNav from '../components/BottomNav';
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+// import React from 'react';
+import { useEffect, useState } from 'react';
+import '../pages/LoginStyles.css';
+// import BottomNav from '../components/BottomNav';
+import { createClient } from '@supabase/supabase-js';
+// import { Auth } from '@supabase/auth-ui-react';
+// import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { useNavigate } from 'react-router-dom';
+
+
+const supabase = createClient(
+    import.meta.env.VITE_REACT_APP_PODS_SUPABASE_URL, 
+    import.meta.env.VITE_REACT_APP_PODS_SUPABASE_ANON_KEY
+  );
 
 export default function Account() {
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <PermIdentityOutlinedIcon />
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
 
-            <span>Manage your account</span>
+    useEffect(() => {
+        async function getUserData() {
+            await supabase.auth.getUser()
+            .then((value) => {
+                // value.data.user
+                if(value.data?.user) {
+                    console.log(value.data.user);
+                    setUser(value.data.user);
+                }
+            })
+        }
+        getUserData();
+    }, []);
 
-            <button>Sign Out</button>
-            <BottomNav />
-        </div>
-    )
+    // sign out user
+    async function signOutUser() {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.log(error)
+        } else {
+            navigate('/login');
+        }
+
+        }
+    
+
+  return (
+      <div className="success--display">
+        { Object.keys(user).length !== 0 ?
+        <>
+            <h2>Success! Welcome to Pods.</h2>
+            <button onClick={() => signOutUser()}>Sign Out</button>
+        </> :
+        <>
+            <h2>User not logged in</h2>
+            <button onClick={() => navigate('/')}>Go back home</button>
+        </>
+        }
+        
+      </div>
+  );
 }
