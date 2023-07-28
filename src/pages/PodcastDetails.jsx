45f5c39f31ec00011d93428632bf250c50b09a65
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-07-28 16:31:12 +0200

    add favourites functionality

--- src/pages/PodcastDetails.jsx
+++ src/pages/PodcastDetails.jsx
@@ -8,23 +8,22 @@ import AudioPlayer from '../components/AudioPlayer';
 import { Link } from "react-router-dom"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from "@mui/icons-material/Favorite" 
-import Favourites from './Favourites';
-
+import { supabase } from '../main';
 
 
 export default function PodcastDetails() {
+    // const user = useUser();
+    const [user, setUser] = useState(null);
     const { id } = useParams();
     const audioRef = useRef();
-
-
+    
     const [selectedPodcast, setSelectedPodcast] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [isPlaying, setIsPlaying] = useState(false);
-    // const [duration, setDuration] = useState(0);
-    // const [isAudioLoaded, setIsAudioLoaded] = useState(true);
     const [currentPlayingEpisodeId, setCurrentPlayingEpisodeId] = useState(null);
     const [episodeFaves, setEpisodeFaves] = useState({})
     const [favourites, setFavourites] = useState([]);
+
 
     // fetch full info about the podcast using the id
     useEffect(() => {
@@ -36,7 +35,24 @@ export default function PodcastDetails() {
             .finally (() => setIsLoading(false));
     }, [id])
 
+     // Fetch user data after the component mounts
+     useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const { data: { user } } = await supabase.auth.getUser();
+                setUser(user);
 
+                // local storage
+                const storedFavourites = localStorage.getItem("favouriteEpisodes");
+                if (storedFavourites) {
+                    setFavourites(JSON.parse(storedFavourites));
+                }
+            } catch (error) {
+                console.error('Error fetching user data from Supabase:', error);
+            }
+        };
+        fetchUserData();
+    }, []);
 
     const handlePlayPause = (episode) => {
         if (isPlaying) {
@@ -47,61 +63,55 @@ export default function PodcastDetails() {
         setIsPlaying((prevState) => !prevState);
       
         // Set the current show details
-        setCurrentPlayingEpisodeId(episode.episode); // ID of the current episode
+        setCurrentPlayingEpisodeId(episode.episode); 
         setSelectedPodcast({
           title: episode.title, 
         });
       };
 
+      // add to array of favourites
+        useEffect(() => {
+            console.log("updated favourites array:", favourites)
+        }, [favourites])
 
-      // switch between the favourite icons. Only show one at a time, interchangeably when clicked on
-      // get the actual selected episode's show id and title
+
+      // switch between the favourite icons, adding and removing favourites
       const handleFavourite = (episode, episodeTitle) => {
         setEpisodeFaves((prevFaves) => ({
-            ...prevFaves,
-            [episode.episode] : !prevFaves[episode.episode],
-
-        }))
-        setFavourites((prevFaves) => [
+          ...prevFaves,
+          [episode.episode]: !prevFaves[episode.episode],
+        }));
+    
+        // Check if the episode is already in the favorites array
+        const isAlreadyFavorite = favourites.some(
+          (item) => item.showId === selectedPodcast.id && item.episodeTitle === episodeTitle
+        );
+    
+        // If the episode is already a favorite, remove it from the favorites array
+        if (isAlreadyFavorite) {
+          setFavourites((prevFaves) =>
+            prevFaves.filter(
+              (item) => !(item.showId === selectedPodcast.id && item.episodeTitle === episodeTitle)
+            )
+          );
+        } else {
+          // Otherwise, add the episode to the favorites array
+          setFavourites((prevFaves) => [
             ...prevFaves,
             {
-                showName: selectedPodcast.title,
-                episodeTitle: episodeTitle,
-                showId: selectedPodcast.id
+              showName: selectedPodcast.title,
+              episodeTitle: episodeTitle,
+              showId: selectedPodcast.id,
+              userId: user
             },
-        ]);
-
-        console.log(episodeTitle, selectedPodcast.title, selectedPodcast.id)
+          ]);
+        }
+        // local storage
+        localStorage.setItem("favouriteEpisodes", JSON.stringify(favourites));
+        console.log(episodeTitle, selectedPodcast.title, selectedPodcast.id);
       };
 
-
-      /*
-      const handleFavourite = (episodeId) => {
-        const isFavourite = episodeFaves[episodeId];
-        
-        if (isFavourite) {
-          setEpisodeFaves((prevFaves) => {
-            const updatedFaves = { ...prevFaves };
-            delete updatedFaves[episodeId];
-            return updatedFaves;
-            
-          });
-
-          setFavouriteEpisodeIds((prevIds) => prevIds.filter((id) => id !== episodeId));
-          console.log(favouriteEpisodeIds)
-        } else {
-          setEpisodeFaves((prevFaves) => ({
-            ...prevFaves,
-            [episodeId]: true,
-          }));
-
-          setFavouriteEpisodeIds((prevIds) => [...prevIds, episodeId]);
-        }
-      };*/
-
-     
-
-    return (
+      return (
         <div>
             {isLoading ? (
                 <div className="loading">
@@ -110,18 +120,18 @@ export default function PodcastDetails() {
             ) : (
             <>
             {selectedPodcast && Object.keys(selectedPodcast).length > 0 && (
-                <div className="details--page">
+                <div className="details--page" key={selectedPodcast.id}>
                     <div className="details--header">
-                        <img src="../public/podcast-bg.png" alt="podcast-background" width={"100%"} className="details--header-image"/>
+                        
+                        <img src="../podcast-bg.png" alt="podcast-background" width={"100%"} style={{ backgroundColor: "var(--light-purple)"}} className="details--header-image"/>
 
-                            <Link to="/search">
-                                <ArrowBackIosIcon 
-                                    className="details--header-back-btn"
-                                    onClick={<Link to="/search"/>}
-                                    // onClick={() => window.location.href = "/search"}
-                                    style={{cursor: "pointer"}}
-                                />
-                            </Link>
+
+                        <Link to="/search" style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <ArrowBackIosIcon
+                            className="details--header-back-btn"
+                            style={{ cursor: "pointer" }}
+                            />
+                        </Link>
 
 
                         <h2 className="details--header-title">{selectedPodcast.title.replace(/&amp;/g, " &")}</h2>
@@ -140,7 +150,6 @@ export default function PodcastDetails() {
                             <aside className="details--season-title"><strong>Season {season.season}</strong> { season.episodes.length > 1 ? `${season.episodes.length} Episodes` : `${season.episodes.length} Episode`}</aside>
                             
                             <div className="details--section-cards">
-                            {/* map through every season's episodes */}
                             {season.episodes.map((episode) => (
                                 <div key={episode.episode} className="details--show-cards">
                                     {isLoading ? (
@@ -165,7 +174,7 @@ export default function PodcastDetails() {
 
                                                     {episodeFaves[episode.episode] ? (
                                                         <FavoriteIcon
-                                                        onClick={() => handleFavourite(episode, episode.title)}
+                                                        onClick={() => handleFavourite(episode, episode.title )}
 
                                                         //  store episode in Favourites list
                                                         className="details--podcast-favourite"
@@ -176,6 +185,7 @@ export default function PodcastDetails() {
                                                         className="details--podcast-favourite"
                                                         />
                                                     )}
+                                                    
 
                                                         <AudioPlayer 
                                                             audioSrc={episode.file} 
@@ -187,13 +197,7 @@ export default function PodcastDetails() {
                                                             currentPodcastImg= {season.image} 
                                                             onPlayPause={handlePlayPause}
                                                         />
-                                                        
-                                                        {/* link to Favourites page */}
-                                                        <Link to="/favourites">
-                                                           <Favourites
-                                                               favourites={favourites}
-                                                           />
-                                                        </Link>
+
                                                         
                                                     </div>
                                                 </div>
